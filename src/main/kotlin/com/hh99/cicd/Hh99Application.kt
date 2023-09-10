4290ef02c41e52122cffd83984cd8ad966a9ad0e
jiseong <jeff3877@gmail.com>
2023-09-10 17:21:30 +0900

    initial commit

---
+++ src/main/kotlin/com/hh99/cicd/Hh99Application.kt
@@ -0,0 +1,11 @@
+package com.hh99.cicd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hh99Application
+
+fun main(args: Array<String>) {
+    runApplication<Hh99Application>(*args)
+}
---
+++ src/test/kotlin/com/hh99/cicd/Hh99ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hh99.cicd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Hh99ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
